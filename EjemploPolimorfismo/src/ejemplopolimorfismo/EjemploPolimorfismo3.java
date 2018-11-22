81a0687067f1be34c02baed80d73da24c78f352b
Mario Valarezo <madx1029@gmail.com>
2018-11-21 22:56:57 -0500

    Ejercicio de tutoria

---
+++ EjemploPolimorfismo/src/ejemplopolimorfismo/EjemploPolimorfismo3.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplopolimorfismo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author reroes
+ */
+public class EjemploPolimorfismo3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ArrayList<PersonaEquipo> integrantes = new ArrayList<PersonaEquipo>();
+        
+        Entrenador delBosque = new Entrenador(1, "Vicente", "Del Bosque", 60, 28489);
+        Futbolista iniesta = new Futbolista(2, "Andres", "Iniesta", 29, 6, "Volante Derecho");
+        Masajista raulMartinez = new Masajista(3, "Raúl", "Martinez", 41, "Licenciado en Fisioterapia");
+        
+        integrantes.add(delBosque);
+        integrantes.add(iniesta);
+        integrantes.add(raulMartinez);
+
+        // ENTRENAMIENTO
+
+        for (PersonaEquipo integrante : integrantes) {
+            integrante.entrenamiento();
+        }
+    }
+
+}
